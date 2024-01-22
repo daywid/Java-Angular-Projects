d48d89d06b538df5d9aed8a4acaa926dae096ae0
Daywid <88506180+daywid@users.noreply.github.com>
2024-01-22 09:34:16 -0300

    list component

--- GerenciadorDeTarefas/src/app/tarefas/tarefas.module.ts
+++ GerenciadorDeTarefas/src/app/tarefas/tarefas.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TarefaService } from './shared'; //Serviços são importados como provider.
+import { TarefaService } from './shared';
+import { ListarTarefaComponent } from './listar'; //Serviços são importados como provider.
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarTarefaComponent
+  ],
   imports: [
     CommonModule
   ],
