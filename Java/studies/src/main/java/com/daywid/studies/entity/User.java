be56b2b297ffdfb8bcb80f6c514c34030689619c
Daywid <88506180+daywid@users.noreply.github.com>
2024-01-23 10:46:14 -0300

    basic project start, JPA and H2 memory

---
+++ Java/studies/src/main/java/com/daywid/studies/entity/User.java
@@ -0,0 +1,39 @@
+package com.daywid.studies.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class User {
+    //Especifica a chave primaria da entidade.
+    @Id 
+    @GeneratedValue(strategy = GenerationType.AUTO) //Gera um auto increment na tabela do banco de dados.
+    private Long id;
+    private String name;
+    private String email;
+
+    @ManyToMany
+    private Set<Role> roles; 
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
