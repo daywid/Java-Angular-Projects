8effa516547e88aa838b06e8cb740c8661be7462
Daywid <88506180+daywid@users.noreply.github.com>
2024-01-25 09:16:17 -0300

    Spring boot API RESTFUL

---
+++ Java Spring/Spring-Studies/src/test/java/com/daywid/Spring/Studies/SpringStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daywid.Spring.Studies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
