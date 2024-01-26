33b4de0df88c7c10a3e46c2ba2eb8d9d5add9dec
Daywid <88506180+daywid@users.noreply.github.com>
2024-01-26 10:30:54 -0300

    Migrations with Flyway

--- Java Spring/Spring-Studies/src/main/java/com/daywid/Spring/Studies/controllers/PersonController.java
+++ Java Spring/Spring-Studies/src/main/java/com/daywid/Spring/Studies/controllers/PersonController.java
@@ -19,7 +19,7 @@ import com.daywid.Spring.Studies.data.vo.v2.PersonVOV2;
 import com.daywid.Spring.Studies.services.PersonServices;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
     
     @Autowired
---
+++ Java Spring/Spring-Studies/src/main/resources/db/migration/V1__Populate_Table_Person.sql
@@ -0,0 +1,3 @@
+INSERT INTO `person` VALUES 
+(3,'Sps','zecasss','Male','aaaaaa'),
+(4,'aaa','teste 4','bb','test');
