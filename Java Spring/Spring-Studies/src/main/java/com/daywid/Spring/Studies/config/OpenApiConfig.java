40389203a465e5c899dd711568d7c209a33ef561
Daywid <88506180+daywid@users.noreply.github.com>
2024-01-29 13:35:34 -0300

    swagger

---
+++ Java Spring/Spring-Studies/src/main/java/com/daywid/Spring/Studies/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.daywid.Spring.Studies.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+    
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+            .info(new Info()
+                .title("RESTful API with Java and Spring")
+                .version("Studies project")
+                .description("")
+                .termsOfService("")
+                .license(new License().name("Apache 2.0")
+                .url("")));
+    }
+
+
+}
