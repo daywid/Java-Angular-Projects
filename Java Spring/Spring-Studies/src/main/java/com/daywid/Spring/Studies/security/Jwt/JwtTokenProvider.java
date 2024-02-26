354573d7db64b5ca1aeead4932d1d13ccb70056c
Daywid Mendes <daywid121@gmail.com>
2024-02-26 13:55:42 -0300

    .

--- Java Spring/Spring-Studies/src/main/java/com/daywid/Spring/Studies/security/Jwt/JwtTokenProvider.java
+++ Java Spring/Spring-Studies/src/main/java/com/daywid/Spring/Studies/security/Jwt/JwtTokenProvider.java
@@ -26,17 +26,16 @@ import jakarta.servlet.http.HttpServletRequest;
 @Service
 public class JwtTokenProvider {
 
-	@Value("${security.jwt.token.secret-key:secret}")
-	private String secretKey = "secret";
+	//@Value("${security.jwt.token.secret-key:secret}")
+	private String secretKey = "53cr37";
 	
-	@Value("${security.jwt.token.expire-length:3600000}")
+	//@Value("${security.jwt.token.expire-length:3600000}")
 	private long validityInMilliseconds = 3600000; // 1h
 
 	Algorithm algorithm = null;
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
 	
 	
 	@PostConstruct
@@ -105,6 +104,7 @@ public class JwtTokenProvider {
 		return decodedJWT;
 	}
 	
+	// bearer token null
 	public String resolveToken(HttpServletRequest req) {
 		String bearerToken = req.getHeader("Authorization");
 		
